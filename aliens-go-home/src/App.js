c1e682b25b95b3431989a10606e244ea51d91aa9
Camila Silveira <26282182+CamilaGS@users.noreply.github.com>
2019-01-31 09:48:53 -0200

    Add files via upload

---
+++ aliens-go-home/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PropTypes from 'prop-types';
+import Canvas from './components/Canvas';
+
+class App extends Component {
+  render() {
+    return (
+      <Canvas />
+    );
+  }
+}
+
+/*App.propTypes = {
+  message: PropTypes.string.isRequired,
+};*/
+
+export default App;
